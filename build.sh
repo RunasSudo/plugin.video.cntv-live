ee310a0190d314a1e6eb9a1d4d0f6f77597985b9
RunasSudo <RunasSudo@gmail.com>
2014-12-17 18:16:09 +1030

    Initial commit.

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+zip -r plugin.video.cntv-live.zip plugin.video.cntv-live/
---
+++ plugin.video.cntv-live/addon.py
@@ -0,0 +1,13 @@
+import sys
+import xbmcgui
+import xbmcplugin
+
+addon_handle = int(sys.argv[1])
+
+xbmcplugin.setContent(addon_handle, 'movies')
+
+url = 'http://localhost/some_video.mkv'
+li = xbmcgui.ListItem('My First Video!', iconImage='DefaultVideo.png')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+xbmcplugin.endOfDirectory(addon_handle)
