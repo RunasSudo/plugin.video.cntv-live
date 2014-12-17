a759aed850b4154cd87abdd85ef3a23dcb18c414
RunasSudo <RunasSudo@gmail.com>
2014-12-17 19:08:18 +1030

    Make addon.

--- plugin.video.cntv-live/addon.py
+++ plugin.video.cntv-live/addon.py
@@ -1,13 +1,26 @@
+# -*- coding: utf-8 -*-
+
 import sys
 import xbmcgui
 import xbmcplugin
+
+import urllib2
 
 addon_handle = int(sys.argv[1])
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
-url = 'http://localhost/some_video.mkv'
-li = xbmcgui.ListItem('My First Video!', iconImage='DefaultVideo.png')
+resp = urllib2.urlopen("http://vdn.live.cntv.cn/api2/liveHtml5.do?channel=pa://cctv_p2p_hdcctv13&client=html5")
+
+data = resp.read().decode("utf-8")
+
+url = data[data.index('"hls3":"') + 8:]
+url = url[:url.index('"')]
+url = url.replace("100-300", "300-500")
+
+print(url)
+
+li = xbmcgui.ListItem('CCTV-13 新闻', iconImage='DefaultVideo.png')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
 xbmcplugin.endOfDirectory(addon_handle)
